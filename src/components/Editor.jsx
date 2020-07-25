e92d3161f5aaf977b66389c1b2419a93fe8d64b7
csy1204 <josang1204@gmail.com>
2020-07-25 23:41:11 +0900

    add api example

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import api from "../lib/api"
 export default function Editor() {
+    const [data, setData] = useState({"results":[]})
+
+    useEffect(()=> {
+        const fetchData = async () => {
+            const result =  await api.getNowPlaying();
+            setData(result.data)
+            // console.log(result);
+        }
+        fetchData();
+    }, [])
+
     return (
         <div>
             <h1> Editor </h1>
+            {data.results.map(d =>(
+                <p>d.title</p>
+            ))}
         </div>
     )
 }

202beed1bfd3b208e2076b50e594af1ff35f10ab
csy1204 <josang1204@gmail.com>
2020-07-25 23:41:27 +0900

    api example in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from "./components/Editor"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Editor />
       </header>
     </div>
   );
